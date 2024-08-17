16f8c26d6583de871a8131b1faaac15e2efde8db
Eric Griffith <eric.griffith42@gmail.com>
2024-08-17 17:14:36 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#![no_std] // don't link the standard Rust library
+#![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use rust_os::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+/// This function is called on a panic, but not for tests
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
+}
